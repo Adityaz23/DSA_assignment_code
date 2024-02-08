840a923b1de2b4cdc65ed1ed0af30aba53c91ea7
Aditya Kumar Soni <95933054+Adityaz23@users.noreply.github.com>
2024-02-08 18:24:05 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Q1. Write a program to print whether a number is even or odd, also take input from the user.
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter some input: ");
+//
+//        int num = input.nextInt(); //this line will check the input is integer or not.
+//
+//        if (num % 2 == 0) {
+//            System.out.println(num + " is an even number.");
+//        } else {
+//            System.out.println(num + " is an odd number");
+//        }
+
+
+        // Q2. Take name as input and print a greeting message for that particular name.
+//        System.out.print("Please enter your name: ");
+//
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.next(); // checking if the input is string or not.
+//        System.out.println("Hello " +input);
+
+
+        // Q3. Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+//        float p,r,t,si;
+//        System.out.print("Please enter the principal value: ");
+//        Scanner principal = new Scanner(System.in);
+//        float pri = principal.nextFloat();
+//
+//        System.out.print("Please enter the rate value: ");
+//        Scanner rate = new Scanner(System.in);
+//        float rat = principal.nextFloat();
+//
+//        System.out.print("Please enter the time value: ");
+//        Scanner time = new Scanner(System.in);
+//        float ti = principal.nextFloat();
+//
+//        si = (pri*rat*ti)/100;
+//        System.out.print("The value of simple interest is: "+si);
+
+        // Q4. Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+
+        // Q5. Take 2 numbers as input and print the largest number.
+//        float a,b;
+//        System.out.print("Please enter the numeric value for a: ");
+//        Scanner sc =  new Scanner(System.in);
+//        float ans = sc.nextFloat();
+//
+//        System.out.print("Please enter the numeric value for b: ");
+//        Scanner sec =  new Scanner(System.in);
+//        float base = sec.nextFloat();
+//
+//        if (ans > base){
+//            System.out.println("The value of a is greater than b: " +ans);
+//        }else {
+//            System.out.println("The value of b is greater than a: " +base);
+//        }
+
+        // Q6. Input currency in rupees and output in USD.
+//        float rup , usd;
+//        System.out.print("Please enter the amount in rupee: ");
+//        Scanner ₹ = new Scanner(System.in);
+//        float rupee = ₹.nextFloat();
+//
+//        float dollar = rupee / 81;
+//        System.out.print("The amount of rupee in dollar is: " +dollar);
+
+
+
+    }
+}
